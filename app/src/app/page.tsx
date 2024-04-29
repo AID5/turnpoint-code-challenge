a318df5d1b3f20c842bf4aa688acfca41c82ef8c
aidenfleming <aiden.l.fleming@gmail.com>
2024-04-29 15:13:30 +1000

    Add modal to alter client data

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
-import React, { Component, useState, useEffect } from "react";
+import React from "react";
 import ClientService from "@/services/client.service";
+import fundingSourceService from "@/services/fundingSource.service";
 import IClientData from "@/types/client.type";
+import ClientModal from "@/components/clientModal";
 
 interface Props {}
 
 interface State {
   clients: Array<IClientData>;
-  newClientData: IClientData;
+  showModal: boolean;
+  selectedClient: IClientData;
 }
 
 class Table extends React.Component<Props, State> {
@@ -15,7 +18,8 @@ class Table extends React.Component<Props, State> {
     super(props);
     this.state = {
       clients: [],
-      newClientData: {
+      showModal: false,
+      selectedClient: {
         id: null,
         firstName: "",
         lastName: "",
@@ -43,7 +47,7 @@ class Table extends React.Component<Props, State> {
       });
   }
 
-  deleteClient(id: number) {
+  onDeleteClient(id: number) {
     ClientService.delete(id)
       .then((response: any) => {
         this.retrieveClients();
@@ -53,90 +57,125 @@ class Table extends React.Component<Props, State> {
       });
   }
 
+  onEditClientClicked(client: IClientData) {
+    this.setState({
+      showModal: true,
+      selectedClient: client,
+    });
+  }
+
+  getFundingSourceNameById(id: number) {
+    fundingSourceService
+      .getNameById(id)
+      .then((response: any) => {
+        return response.data;
+      })
+      .catch((e: Error) => {
+        console.log(e);
+      });
+  }
+
+  closeModal() {
+    this.setState({
+      showModal: false,
+    });
+  }
+
   render() {
     return (
-      <main className="flex min-h-screen min-w-full bg-gray-950 items-center justify-center">
-        <div>
-          <span className="text-white font-bold">Clients</span>
-          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm rtl:text-right text-gray-500 dark:text-gray-400 text-center">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th scope="col" className="px-6 py-3">
-                    Name
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    DOB
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Primary Language
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Secondary Language
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Primary Funding Source
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Action
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.clients.map((client, index) => {
-                  return (
-                    <tr
-                      key={index}
-                      className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
-                    >
-                      <td className="px-6 py-4">
-                        {client.firstName} {client.lastName}
-                      </td>
-                      <td className="px-6 py-4">
-                        {client.dob.toLocaleString()}
-                      </td>
-                      <td className="px-6 py-4">{client.primaryLanguage}</td>
-                      <td className="px-6 py-4">{client.secondaryLanguage}</td>
-                      <td className="px-6 py-4">
-                        {client.primaryFundingSourceId}
-                      </td>
-                      <td className="px-6 py-4">
-                        <div className="flex row justify-center">
-                          <button
-                            data-modal-target="default-modal"
-                            data-modal-toggle="default-modal"
-                            className="block mx-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                            type="button"
-                          >
-                            Edit
-                          </button>
-                          <button
-                            data-modal-target="default-modal"
-                            data-modal-toggle="default-modal"
-                            className="block mx-1 text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                            type="button"
-                            onClick={() => this.deleteClient(client.id!)}
-                          >
-                            Delete
-                          </button>
-                        </div>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-            <button
-              data-modal-target="default-modal"
-              data-modal-toggle="default-modal"
-              className="block mx-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-              type="button"
-            >
-              Add new
-            </button>
+      <div>
+        <ClientModal
+          showModal={this.state.showModal}
+          client={this.state.selectedClient}
+          closeModal={this.closeModal}
+        />
+
+        <main className="flex min-h-screen min-w-full bg-gray-950 items-center justify-center">
+          <div>
+            <span className="text-white font-bold">Clients</span>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+              <table className="w-full text-sm rtl:text-right text-gray-500 dark:text-gray-400 text-center">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      Name
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      DOB
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Primary Language
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Secondary Language
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Primary Funding Source
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.clients.map((client, index) => {
+                    return (
+                      <tr
+                        key={index}
+                        className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+                      >
+                        <td className="px-6 py-4">
+                          {client.firstName} {client.lastName}
+                        </td>
+                        <td className="px-6 py-4">
+                          {client.dob.toLocaleString()}
+                        </td>
+                        <td className="px-6 py-4">{client.primaryLanguage}</td>
+                        <td className="px-6 py-4">
+                          {client.secondaryLanguage}
+                        </td>
+                        <td className="px-6 py-4">
+                          {client.primaryFundingSourceId}
+                        </td>
+                        <td className="px-6 py-4">
+                          <div className="flex row justify-center">
+                            <button
+                              data-modal-target="default-modal"
+                              data-modal-toggle="default-modal"
+                              className="block mx-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                              type="button"
+                              onClick={() => this.onEditClientClicked(client!)}
+                            >
+                              Edit
+                            </button>
+                            <button
+                              data-modal-target="default-modal"
+                              data-modal-toggle="default-modal"
+                              className="block mx-1 text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                              type="button"
+                              onClick={() => this.onDeleteClient(client.id!)}
+                            >
+                              Delete
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+              <button
+                data-modal-target="default-modal"
+                data-modal-toggle="default-modal"
+                className="block mx-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                type="button"
+              >
+                Add new
+              </button>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
     );
   }
 }
---
+++ app/src/components/clientModal.tsx
@@ -0,0 +1,141 @@
+import React from "react";
+import ClientService from "@/services/client.service";
+import fundingSourceService from "@/services/fundingSource.service";
+import IClientData from "@/types/client.type";
+
+interface Props {
+  showModal: boolean;
+  client: IClientData;
+  closeModal: any;
+}
+
+interface State {}
+
+class ClientModal extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    console.log(this.props.showModal);
+  }
+  closeModal() {
+    this.props.closeModal();
+  }
+  editClient(client: IClientData) {
+    ClientService.update(client, client.id!)
+      .then((response: any) => {
+        console.log(response);
+      })
+      .catch((e: Error) => {
+        console.log(e);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <div
+          id="default-modal"
+          tabIndex={-1}
+          aria-hidden="true"
+          className={`overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full
+          ${this.props.showModal ? "flex" : "hidden"}`}
+        >
+          <div className="relative p-4 w-full max-w-2xl max-h-full">
+            {/* <!-- Modal content --> */}
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              {/* <!-- Modal header --> */}
+              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                  Client
+                </h3>
+                <button
+                  type="button"
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-hide="default-modal"
+                  onClick={() => this.closeModal()}
+                >
+                  <svg
+                    className="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+              {/* <!-- Modal body --> */}
+              <div className="p-4 md:p-5 space-y-4">
+                <div className="grid md:grid-cols-2 md:gap-6">
+                  <div className="relative z-0 w-full mb-5 group">
+                    <input
+                      type="text"
+                      name="floating_first_name"
+                      id="floating_first_name"
+                      className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                      placeholder=" "
+                      required
+                      defaultValue={this.props.client.firstName}
+                    />
+                    <label
+                      htmlFor="floating_first_name"
+                      className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                    >
+                      First name
+                    </label>
+                  </div>
+                  <div className="relative z-0 w-full mb-5 group">
+                    <input
+                      type="text"
+                      name="floating_last_name"
+                      id="floating_last_name"
+                      className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                      placeholder=" "
+                      required
+                      defaultValue={this.props.client.lastName}
+                    />
+                    <label
+                      htmlFor="floating_last_name"
+                      className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                    >
+                      Last name
+                    </label>
+                  </div>
+                </div>
+              </div>
+              {/* <!-- Modal footer --> */}
+              <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button
+                  data-modal-hide="default-modal"
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  onClick={() => this.editClient(this.props.client)}
+                >
+                  Submit
+                </button>
+                <button
+                  data-modal-hide="default-modal"
+                  type="button"
+                  className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                >
+                  Discard
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ClientModal;
